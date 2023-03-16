53bd7a512a2a4b6d9426f5d8aa78848df109fa0b
sifeddine-work <sifeddin.dalal@gmail.com>
2023-03-16 16:39:31 -0700

    5 . Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A c program that print a Text with printf function
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
