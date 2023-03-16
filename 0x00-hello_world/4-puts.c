a396b0a0d90dd4815800b4fcb550a738aabaac18
sifeddine-work <sifeddin.dalal@gmail.com>
2023-03-16 15:53:33 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *   main : A c program that print a Text with puts function
+ *
+ *   return : 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0 ;
+}
