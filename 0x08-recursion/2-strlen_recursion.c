96d6d38b84a573059611f055ce34c82b7a7a5870
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-05 11:44:25 -0700

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: pointer to character (string);
+ * Return: integer(length of string)
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		s++;
+		len++;
+		return (len + _strlen_recursion(s));
+	}
+}
