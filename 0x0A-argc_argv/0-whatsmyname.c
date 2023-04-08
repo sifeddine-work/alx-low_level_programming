e122b5f08b5b666441b70299c56593848b98cbfe
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-08 10:08:44 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints its name.
+ * @argc: Number of command line arguments
+ * @argv: tabla point to character
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# 0x0A. C - argc, argv #
+- WELCOME TO THE C - argc, argv
