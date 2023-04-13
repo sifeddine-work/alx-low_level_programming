d077541df27aa0729b24078b9de5c6e28686beb8
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-13 08:58:24 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,13 @@ int *array_range(int min, int max)
 	int *ptr;
 	int i, size;
 
-	if (min < max)
+	if (min > max)
 		return (NULL);
 	size = max - min + 1;
 	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; min <= size; i++)
+	for (i = 0; min <= max; i++)
 		ptr[i] = min++;
 	return (ptr);
 }
