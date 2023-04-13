e36b7b4a12f984ee34d559a28767350b1968f769
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-13 08:46:25 -0700

    3. array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *array_range - function that creates an array of integers
+ * @min: integer
+ * @max: integer
+ * Return: pinter to int (array)
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, size;
+
+	if (min < max)
+		return (NULL);
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
