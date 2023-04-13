4e751a53f98e675ac91be336a9855b39fc66b4a6
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-13 07:46:28 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,35 +10,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	int i, j, size_s1 = 0, size_s2 = 0;
+	unsigned int i, j, k;
 
 	if (s1 == NULL)
 		return (NULL);
 
 	if (s1 == NULL)
-		s1 = "";
+		i = 0;
 	else
 	{
-		for (; s1[size_s1] != '\0'; size_s1++)
+		for (i = 0; s1[i]; ++i)
 		;
 	}
 
 	if (s2 == NULL)
-		s2 = "";
+		j = 0;
 	else
 	{
-		for (; s2[size_s2] != '\0'; size_s2++)
+		for (j = 0; s2[j]; ++j)
 		;
 	}
-
-	s = malloc(sizeof(char) * (size_s1 + n) + 1);
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
 	if (s == NULL)
 		return (NULL);
 
-	for (j = 0; j < size_s1; j++)
-		s[j] = s1[j];
-	for (i = 0; i < size_s2; i++)
-		s[i + size_s1] = s2[i];
-	s[n + size_s1] = '\0';
+	for (k = 0; k < size_s1; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
 	return (s);
 }
