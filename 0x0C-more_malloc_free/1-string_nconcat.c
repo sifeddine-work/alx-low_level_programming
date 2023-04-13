5c1ceb088db2d81a41a07cb26ab979ed5ff0cacc
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-13 07:50:10 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s == NULL)
 		return (NULL);
 
-	for (k = 0; k < size_s1; k++)
+	for (k = 0; k < i; k++)
 		s[k] = s1[k];
 	for (k = 0; k < j; k++)
 		s[k + i] = s2[k];
