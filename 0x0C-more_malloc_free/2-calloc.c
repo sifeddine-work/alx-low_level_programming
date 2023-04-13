dd54e7b5866537991a661ea0e1d1664eada9209c
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-13 08:24:31 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,7 +28,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	m = malloc(sizeof(sizeof(int) * nmemb);
+	m = malloc(sizeof(int) * nmemb);
 	if (m == 0)
 		return (NULL);
 	_memset(m, 0, sizeof(int) * nmemb);
