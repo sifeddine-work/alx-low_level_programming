65689833cb877ca1dbfc2d976d526ef2fbf76372
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-27 08:01:08 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,12 +4,13 @@
  * print_list - function that prints all the elements of a list
  * @h: pointer to list
  *
- * Return: numner of nodes pointed 
+ * Return: numner of nodes pointed
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t t = 0;
+
 	while (h)
 	{
 		if (h->str == NULL)
