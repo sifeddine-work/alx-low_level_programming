1cb1093a06d4627e49b99636aa498aafeeb72339
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-27 08:24:28 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number
+	of elements in a linked
+ * @h: pointer to lists
+ *
+ * Retuen: nember of element in a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
