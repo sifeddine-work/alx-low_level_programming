1bf1243d75390644f7fde3e99290cb907c2b0fe9
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-29 11:22:21 -0700

    6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - unction that deletes and return data of the head node of a list
+ * pop_listint - unction that deletes and return data of a list
  * @head: pointer to head of a list
  *
  * Return: int
@@ -15,7 +15,10 @@ int pop_listint(listint_t **head)
 	if (current == NULL)
 		return (0);
 
-	*head = (*head)->next;
+	(*head) = (*head)->next;
+
+	if (*head == NULL)
+		return(0);
 
 	return (current->n);
 
