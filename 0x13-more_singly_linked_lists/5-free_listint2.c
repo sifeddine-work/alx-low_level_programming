73c2316f94302a287a881205f1c31569202f1524
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-29 10:46:59 -0700

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,15 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		current = (*head)->next;
 		free(*head);
 		*head = current;
 	}
-
-	if (head == NULL)
-		return;
 
 	*head = NULL;
 }
