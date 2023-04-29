a3d17e6c55c4ec1df9c18feeed44482704b6c136
sifeddine-work <sifeddin.dalal@gmail.com>
2023-04-29 10:39:21 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pinter to head of a list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
+}
+
